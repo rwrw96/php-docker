ac107e9481c7ecf6345a40d71ded4657b47f27e3
rwrw96 <kazuki137@gmail.com>
2021-08-18 08:07:30 +0900

    jquery 点滅

--- docker_file_test/test/index.js
+++ docker_file_test/test/index.js
@@ -1,1 +1,27 @@
-alert('こんにちは');
+
+// alert('こんにちは');
+var Member = function(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getName = function(){
+        return this.lastName + this.firstName;
+    }
+    
+};
+    var mem = new Member('test', 'query');
+    console.log(mem.getName());
+
+// var current = new Date();
+// var result = document.getElementById('result');
+// result.textContent = current.toLocaleString();
+
+$(function(){
+    $('.box').hover(
+        function(){
+            $(this).fadeOut();
+        },
+        function(){
+            $(this).fadeIn();
+        }
+    );
+})
